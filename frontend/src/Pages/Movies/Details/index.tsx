f5da14f377919995f3fd58f92bec242dbb17f71e
Renan <souza.rnn@gmail.com>
2021-07-23 14:28:25 -0300

    Frontend List reviews by movie id, save a new review

--- frontend/src/Pages/Movies/Details/index.tsx
+++ frontend/src/Pages/Movies/Details/index.tsx
@@ -4,31 +4,71 @@ import { useParams } from "react-router-dom";
 import { ReactComponent as IconStar } from "../../../assets/images/icon-star.svg";
 import { Movie } from "../../../types/movie";
 import { hasAnyRoles, requestBackend } from "../../../util/requests";
+import { useForm } from "react-hook-form";
 import "./styles.css";
 
 type UrlParams = {
   movieId: string;
-}
+};
+
+type FormData = {
+  review: string;
+};
 
 const Details = () => {
-
   const { movieId } = useParams<UrlParams>();
 
   const [movie, setMovie] = useState<Movie[]>();
 
+  const [hasError, setHasError] = useState(false);
+
+  const [refresh, setRefresh] = useState(false);
 
   useEffect(() => {
     const params: AxiosRequestConfig = {
-      method: 'GET',
+      method: "GET",
       url: `/movies/${movieId}/reviews`,
       withCredentials: true,
     };
 
+    requestBackend(params).then((response) => {
+      setMovie(response.data);
+    });
+  }, [movieId, refresh]);
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const onSubmit = (formData: FormData) => {
+    const params: AxiosRequestConfig = {
+      method: "POST",
+      url: "/reviews",
+      withCredentials: true,
+      data: {
+        text: formData.review,
+        movieId: movieId,
+      },
+    };
+
     requestBackend(params)
-      .then((response) => {
-        setMovie(response.data);
+      .then(() => {
+        setRefresh(true);
+        reset();
+        setHasError(false);
+        alert("Comentário salvo com sucesso");
       })
-  }, [movieId]);
+      .catch((error) => {
+        setHasError(true);
+        setRefresh(false);
+      })
+      .finally(() => {
+        setRefresh(false);
+      });
+  };
 
   return (
     <div className="movies-details-container">
@@ -37,51 +77,49 @@ const Details = () => {
       </div>
       {hasAnyRoles(["ROLE_MEMBER"]) && (
         <div className="card bg-secondary-light shadow mt-3">
-          <div className="card-body">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Deixe sua avaliação aqui"
-            />
-            <div className="movies-details-btn-container">
-              <button className="btn btn-primary movies-details-btn">
-                SALVAR AVALIAÇÃO
-              </button>
+          {hasError && (
+            <div className="alert alert-danger">
+              Erro ao salvar o comentário
             </div>
+          )}
+
+          <div className="card-body">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <input
+                {...register("review", {
+                  required: "Campo obrigatório",
+                })}
+                type="text"
+                className={`form-control ${errors.review ? "is-invalid" : ""}`}
+                placeholder="Deixe sua avaliação aqui"
+                name="review"
+              />
+              <small className="text-danger"> {errors.review?.message} </small>
+              <div className="movies-details-btn-container">
+                <button className="btn btn-primary movies-details-btn">
+                  SALVAR AVALIAÇÃO
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       )}
 
       <div className="card bg-secondary-light shadow mt-3">
         <div className="card-body">
-
-        {movie?.map((item) => (
-          <div key={item.id}>
+          {movie?.map((item) => (
+            <div key={item.id}>
               <div className="movies-details-coments-header mt-4">
-                <IconStar /> <span className="text-white">{item.user.name}</span>
+                <IconStar />{" "}
+                <span className="text-white">{item.user.name}</span>
               </div>
               <div className="movies-details-coments-content">
-                <p>
-                  {item.text}
-                </p>
+                <p>{item.text}</p>
               </div>
-          </div>
-            
-        ))}
-
-              
-       
-
-              
-
-          
-
-
+            </div>
+          ))}
         </div>
       </div>
-
-     
-  
     </div>
   );
 };
